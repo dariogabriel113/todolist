5244935118e471ef3ea77f3c9dde2fb8f6150683
Dario Gabriel <dariogabriel113@gmail.com>
2024-05-24 10:15:18 -0300

    Criação do controller TodoController com os metodos básicos do CRUD

---
+++ src/main/java/com/dario/todoList/TodoList/controller/TodoController.java
@@ -0,0 +1,38 @@
+package com.dario.todoList.TodoList.controller;
+
+import com.dario.todoList.TodoList.model.Todo;
+import com.dario.todoList.TodoList.service.TodoService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/todos")
+public class TodoController {
+
+    private TodoService todoService;
+
+    public TodoController(TodoService todoService) {
+        this.todoService = todoService;
+    }
+
+    @GetMapping
+    List<Todo> listar() {
+        return todoService.listar();
+    }
+
+    @PostMapping
+    List<Todo> criar(@RequestBody Todo todo) {
+        return todoService.criar(todo);
+    }
+
+    @PutMapping
+    List<Todo> editar(@RequestBody Todo todo) {
+        return todoService.editar(todo);
+    }
+
+    @DeleteMapping("{id}")
+    List<Todo> apagar(@PathVariable("id") Long id) {
+        return todoService.apagar(id);
+    }
+}
