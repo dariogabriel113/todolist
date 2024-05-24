c92e2c383c1ea3d31707f145fafac1f26bebe2dc
Dario Gabriel <dariogabriel113@gmail.com>
2024-05-24 10:52:55 -0300

    Todo - Correção na geração de id para a entidade

--- src/main/java/com/dario/todoList/TodoList/model/Todo.java
+++ src/main/java/com/dario/todoList/TodoList/model/Todo.java
@@ -6,7 +6,7 @@ import jakarta.persistence.*;
 @Table(name="todos")
 public class Todo {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String titulo;
